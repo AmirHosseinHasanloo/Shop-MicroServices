1d059a93510a0db3b9f9aa4ee3f8d1e25bc3a233
AmirHosseinHasanloo <amirhosseinhasanloo2005@gmail.com>
2025-02-21 21:58:19 +0330

    mongodb Database configured

---
+++ Services/Catalog/Catalog.API/Context/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Context
+{
+    public class CatalogContext : ICatalogContext
+    {
+        
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(IConfiguration _configuration)
+        {
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var dataBase = client.GetDatabase(_configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = dataBase.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeedData.seedData(Products);
+        }
+       
+       
+    }
+}
---
+++ Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "فیلد {0} نمی تواند بیش از {1} کاراکتر باشد.")]
+        public string Name { get; set; }
+        [Display(Name = "گروه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "فیلد {0} نمی تواند بیش از {1} کاراکتر باشد.")]
+        public string Category { get; set; }
+        [Display(Name = "خلاصه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(450, ErrorMessage = "فیلد {0} نمی تواند بیش از {1} کاراکتر باشد.")]
+        public string Summary { get; set; }
+        [Display(Name = "توضیحات")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(1200, ErrorMessage = "فیلد {0} نمی تواند بیش از {1} کاراکتر باشد.")]
+        public string Description { get; set; }
+        [Display(Name = "آواتار")]
+        public string ImageName { get; set; }
+        [Display(Name = "قیمت")]
+        public decimal Price { get; set; }
+    }
+}
