9db832476dd6a911d7040f2a9233b3fda6e9a64d
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-04-23 16:18:40 +0330

    Get Discount grpc is added + AutoMapper

--- Services/DiscountAPI/DiscountGrpc/Program.cs
+++ Services/DiscountAPI/DiscountGrpc/Program.cs
@@ -1,11 +1,13 @@
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Extentions;
+using Discount.Grpc.Services;
 //using DiscountGrpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 #region IOC
 
@@ -19,7 +21,7 @@ var app = builder.Build();
 app.MigrateDataBase<Program>();
 
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ Services/DiscountAPI/DiscountGrpc/Services/DiscountService.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.CompilerServices;
+using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        #region Constractor
+
+        private readonly IDiscountRepository _discountRepository;
+        private readonly ILogger<DiscountRepository> _logger;
+        private readonly IMapper _mapper;
+
+        public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountRepository> logger, IMapper mapper)
+        {
+            _discountRepository = discountRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        #region Get Discount
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _discountRepository.GetDiscount(request.ProductName);
+
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"There is no discount with {request.ProductName} product name!"));
+            }
+
+            _logger.LogInformation("Discount is retrived for product name !");
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        #endregion
+    }
+}
