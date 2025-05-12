bb65c37ebceb6275aa57ba528c89147df229fda8
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-05-12 21:12:03 +0330

    initialize Common at domain and some entity

---
+++ Services/Ordering.API/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public string UserName { get; set; }
+
+        //TODO : Add Address class
+
+        //TODO : Add ContanctInfo class
+
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        // payment method
+        public string CardName { get; set; }
+        public string RefCode { get; set; }
+        public int PaymentMethod { get; set; }
+
+    }
+
+}
+
