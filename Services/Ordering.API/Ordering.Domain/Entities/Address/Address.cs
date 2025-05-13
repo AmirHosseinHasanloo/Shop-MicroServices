b01f8137cc96d973ca4ff2d8fff0a9cc34d04993
AmirHosseinHasanloo <AmirHosseinHasanloo2005@gmail.com>
2025-05-13 17:32:28 +0330

    Generic Repo and IOrder Repo is add up

---
+++ Services/Ordering.API/Ordering.Domain/Entities/Address/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities.Order;
+
+namespace Ordering.Domain.Entities.Address
+{
+    public class Address : EntityBase
+    {
+        public string UserAddress { get; set; }
+        public int OrderId { get; set; }
+        Order.Order order { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
--- Services/Ordering.API/Ordering.Domain/Entities/Order/Order.cs
+++ Services/Ordering.API/Ordering.Domain/Entities/Order/Order.cs
@@ -4,28 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ordering.Domain.Common;
+using Ordering.Domain.Entities.Payment;
 
-namespace Ordering.Domain.Entities
+namespace Ordering.Domain.Entities.Order
 {
     public class Order : EntityBase
     {
         public string UserName { get; set; }
 
-        //TODO : Add Address class
-
-        //TODO : Add ContanctInfo class
+        public ICollection<Address.Address> Addresses { get; set; }
 
         public decimal TotalPrice { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-
-        // payment method
         public string CardName { get; set; }
         public string RefCode { get; set; }
-        public int PaymentMethod { get; set; }
+        PaymentMethod PaymentMethod { get; set; }
+
 
     }
 
